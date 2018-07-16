e1c9ad7d66548ecec6951ab4d836c307ecabfe05
elvisXJ <1183574425@qq.com>
2018-07-16 09:04:04 -0700

    add build file, redirect libs output path

---
+++ build/math/src/CMakeFiles/mathfunctions.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mathfunctions.dir/math.cpp.o"
+  "../../../libs/libmathfunctions.pdb"
+  "../../../libs/libmathfunctions.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mathfunctions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
